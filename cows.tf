385806f3cb4f851bc94cc95e0f868912b564c807
Admir Trakic <adtr@dr.dk>
2021-12-30 14:12:43 +0100

    Skip cows.tf

--- cows.tf
+++ cows.tf
@@ -1,4 +1,7 @@
+/*
+Hitting: https://github.com/hashicorp/terraform-provider-kubernetes/issues/1367
 resource "kubernetes_manifest" "openfaas_fn_showcow" {
   depends_on = [kubernetes_namespace.openfaas, helm_release.openfaas]
   manifest = yamldecode(file("${path.module}/functions/cows/cows.yaml"))
 }
+*/
